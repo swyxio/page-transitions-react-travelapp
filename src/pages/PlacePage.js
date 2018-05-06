38eb600d5e51050985c3d5d88197b206b80ad2e6
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-05-06 03:16:18 -0400

    use functions cos they're cool

--- src/pages/PlacePage.js
+++ src/pages/PlacePage.js
@@ -5,47 +5,45 @@ import AppStarRating from '../components/AppStarRating';
 import AppFooter from '../components/AppFooter.js';
 import { Subscribe, State } from '../store';
 
-export default class extends React.Component {
-  render() {
-    return (
-      <Subscribe to={[State]}>
-        {$ => (
-          <React.Fragment>
-            <main className="PlaceMain">
-              <div className="places">
-                <p className="top">{$.selectedUser().name}'s Places</p>
-                <h1>{places[0].name}</h1>
-                <p>
-                  <strong>Rating: {places[0].rating}</strong>
-                </p>
-                <div className="stars">
-                  <AppStarRating />
-                </div>
-
-                <div className="main-img" />
-                <p>{places[0].description}</p>
+export default function PlacePage() {
+  return (
+    <Subscribe to={[State]}>
+      {$ => (
+        <React.Fragment>
+          <main className="PlaceMain">
+            <div className="places">
+              <p className="top">{$.selectedUser().name}'s Places</p>
+              <h1>{places[0].name}</h1>
+              <p>
+                <strong>Rating: {places[0].rating}</strong>
+              </p>
+              <div className="stars">
+                <AppStarRating />
               </div>
 
-              <aside className="sidebar">
-                <h3>Other Trips</h3>
-                {places.map(place => {
-                  return (
-                    <div className="placelocation" key={place.name}>
-                      <img src={place.img} alt={place.name} />
-                      <p className="top">
-                        <strong>{place.name}</strong>
-                      </p>
-                      <p>{place.description}</p>
-                      <hr />
-                    </div>
-                  );
-                })}
-              </aside>
-            </main>
-            <AppFooter />
-          </React.Fragment>
-        )}
-      </Subscribe>
-    );
-  }
+              <div className="main-img" />
+              <p>{places[0].description}</p>
+            </div>
+
+            <aside className="sidebar">
+              <h3>Other Trips</h3>
+              {places.map(place => {
+                return (
+                  <div className="placelocation" key={place.name}>
+                    <img src={place.img} alt={place.name} />
+                    <p className="top">
+                      <strong>{place.name}</strong>
+                    </p>
+                    <p>{place.description}</p>
+                    <hr />
+                  </div>
+                );
+              })}
+            </aside>
+          </main>
+          <AppFooter />
+        </React.Fragment>
+      )}
+    </Subscribe>
+  );
 }
