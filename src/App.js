7df51490e64bac3722d13f19a7fa81b54226d360
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-05-06 03:15:04 -0400

    clean up app page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import AppNavigation from './components/AppNavigation.js';
 import IndexPage from './pages/IndexPage.js';
 import GroupPage from './pages/GroupPage.js';
 import PlacePage from './pages/PlacePage.js';
 import { Provider } from 'unstated';
-import { Subscribe, State } from './store';
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Provider>
-          <AppNavigation />
-          <Subscribe to={[State]}>
-            {$ => (
-              <TransitionGroup exit={false}>
-                <CSSTransition key={window.location} classNames="page" timeout={1000}>
-                  <Switch>
-                    <Route exact path="/" render={() => <IndexPage />} />
-                    <Route path="/group" render={() => <GroupPage />} />
-                    <Route path="/place" render={() => <PlacePage currentUser={$.selectedUser()} />} />
-                  </Switch>
-                </CSSTransition>
-              </TransitionGroup>
-            )}
-          </Subscribe>
-        </Provider>
-      </Router>
-    );
-  }
+export default function App() {
+  return (
+    <Router>
+      <Provider>
+        <AppNavigation />
+        <TransitionGroup exit={false}>
+          <CSSTransition key={window.location} classNames="page" timeout={1000}>
+            <Switch>
+              <Route exact path="/" component={IndexPage} />
+              <Route path="/group" component={GroupPage} />
+              <Route path="/place" component={PlacePage} />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      </Provider>
+    </Router>
+  );
 }
-
-export default App;
--- src/pages/PlacePage.js
+++ src/pages/PlacePage.js
@@ -1,43 +1,51 @@
-
-import React from 'react'
+import React from 'react';
 import { places } from '../store';
-import './PlacePage.css'
-import AppStarRating from '../components/AppStarRating'
-import AppFooter from '../components/AppFooter.js'
+import './PlacePage.css';
+import AppStarRating from '../components/AppStarRating';
+import AppFooter from '../components/AppFooter.js';
+import { Subscribe, State } from '../store';
+
 export default class extends React.Component {
-
   render() {
-    const { currentUser } = this.props
     return (
-      <React.Fragment>
-        <main className="PlaceMain">
-          <div className="places">
-            <p className="top">{currentUser.name}'s Places</p>
-            <h1>{places[0].name}</h1>
-            <p><strong>Rating: {places[0].rating}</strong></p>
-            <div className="stars"><AppStarRating /></div>
-
-            <div className="main-img"></div>
-            <p>{places[0].description}</p>
-          </div>
-
-          <aside className="sidebar">
-            <h3>Other Trips</h3>
-            {places.map(place => {
-              return (
-                <div className="placelocation" key={place.name}>
-                  <img src={place.img} alt={place.name} />
-                  <p className="top"><strong>{place.name}</strong></p>
-                  <p>{place.description}</p>
-                  <hr />
+      <Subscribe to={[State]}>
+        {$ => (
+          <React.Fragment>
+            <main className="PlaceMain">
+              <div className="places">
+                <p className="top">{$.selectedUser().name}'s Places</p>
+                <h1>{places[0].name}</h1>
+                <p>
+                  <strong>Rating: {places[0].rating}</strong>
+                </p>
+                <div className="stars">
+                  <AppStarRating />
                 </div>
-              )
-            })}
-          </aside>
 
-        </main>
-        <AppFooter />
-      </React.Fragment>
-    )
+                <div className="main-img" />
+                <p>{places[0].description}</p>
+              </div>
+
+              <aside className="sidebar">
+                <h3>Other Trips</h3>
+                {places.map(place => {
+                  return (
+                    <div className="placelocation" key={place.name}>
+                      <img src={place.img} alt={place.name} />
+                      <p className="top">
+                        <strong>{place.name}</strong>
+                      </p>
+                      <p>{place.description}</p>
+                      <hr />
+                    </div>
+                  );
+                })}
+              </aside>
+            </main>
+            <AppFooter />
+          </React.Fragment>
+        )}
+      </Subscribe>
+    );
   }
 }
